028540a0d6b7fec9d09ebd290d1ee67b7a2f7ea2
TIit0 <viverlin7@gmail.com>
2022-07-04 16:41:09 -0500

    adeed responsive and id links- still needs email data validation

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("hi")
+
+const hamburger = document.getElementById("hamburger");
+const navList = document.getElementById("navList");
+console.log(hamburger);
+console.log(navList);
+
+hamburger.addEventListener("click", () => {
+hamburger.classList.toggle("active");
+navList.classList.toggle("active");
+});
+
+
+document.querySelectorAll(".nav__list__item--link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navList.classList.remove("active");
+}));
